124090992e30120c0089db44564dcf6be5908252
Andy Stamp <luigi88s@gmail.com>
2015-09-28 21:22:25 -0400

    pyxt.helpers: Add tests for addresses past 0xFFFF that do not wrap into the A20 line.

--- pyxt/tests/test_helpers.py
+++ pyxt/tests/test_helpers.py
@@ -12,6 +12,15 @@ class SegmentOffsetToAddressTests(unittest.TestCase):
     def test_segment_offset_to_address_all_offset(self):
         self.assertEqual(segment_offset_to_address(0x0000, 0x8124), 0x8124)
         
+    def test_segment_offset_to_address_reset_vector(self):
+        self.assertEqual(segment_offset_to_address(0xFFFF, 0x0000), 0xFFFF0)
+        
+    def test_segment_offset_to_address_machine_id(self):
+        self.assertEqual(segment_offset_to_address(0xF000, 0xFFFE), 0xFFFFE)
+        
+    def test_segment_offset_to_address_machine_id2(self):
+        self.assertEqual(segment_offset_to_address(0xFFFF, 0x000E), 0xFFFFE)
+        
     def test_segment_offset_to_address_wrap(self):
         self.assertEqual(segment_offset_to_address(0xFFFF, 0xFFFF), 0xFFEF)
         
