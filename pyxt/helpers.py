0a32a79041cb21915ee96a64c8b46ee657a2d612
Andy Stamp <luigi88s@gmail.com>
2015-10-05 23:35:11 -0400

    pyxt.helpers: Add word_to_bytes and bytes_to_word helpers.

--- pyxt/helpers.py
+++ pyxt/helpers.py
@@ -6,3 +6,15 @@ def segment_offset_to_address(segment, offset):
     """ Convert a segment and offset to a real address. """
     return ((segment << 4) + offset) & 0xFFFFF
     
+def word_to_bytes(value):
+    """ Convert a word into a tuple of 2 bytes. """
+    if value < 0 or value > 0xFFFF:
+        raise ValueError("value must be in the range [0, 0xFFFF]!")
+    return (value & 0x00FF), ((value & 0xFF00) >> 8)
+    
+def bytes_to_word(data):
+    """ Convert a sequence of 2 bytes into a word. """
+    if len(data) != 2:
+        raise ValueError("data must be a sequence of 2 bytes!")
+    return ((data[1] & 0xFF) << 8) | (data[0] & 0xFF)
+    
--- pyxt/tests/test_helpers.py
+++ pyxt/tests/test_helpers.py
@@ -1,26 +1,54 @@
 import unittest
 
-from pyxt.helpers import segment_offset_to_address
+from pyxt.helpers import *
 
 class SegmentOffsetToAddressTests(unittest.TestCase):
-    def test_segment_offset_to_address_simple(self):
+    def test_simple(self):
         self.assertEqual(segment_offset_to_address(0x06EF, 0x1234), 0x8124)
         
-    def test_segment_offset_to_address_high_segment(self):
+    def test_high_segment(self):
         self.assertEqual(segment_offset_to_address(0x0812, 0x0004), 0x8124)
         
-    def test_segment_offset_to_address_all_offset(self):
+    def test_all_offset(self):
         self.assertEqual(segment_offset_to_address(0x0000, 0x8124), 0x8124)
         
-    def test_segment_offset_to_address_reset_vector(self):
+    def test_reset_vector(self):
         self.assertEqual(segment_offset_to_address(0xFFFF, 0x0000), 0xFFFF0)
         
-    def test_segment_offset_to_address_machine_id(self):
+    def test_machine_id(self):
         self.assertEqual(segment_offset_to_address(0xF000, 0xFFFE), 0xFFFFE)
         
-    def test_segment_offset_to_address_machine_id2(self):
+    def test_machine_id2(self):
         self.assertEqual(segment_offset_to_address(0xFFFF, 0x000E), 0xFFFFE)
         
-    def test_segment_offset_to_address_wrap(self):
+    def test_wrap(self):
         self.assertEqual(segment_offset_to_address(0xFFFF, 0xFFFF), 0xFFEF)
         
+class BytesToWordTests(unittest.TestCase):
+    def test_simple(self):
+        self.assertEqual(bytes_to_word((0x56, 0x43)), 0x4356)
+        
+    def test_too_few(self):
+        with self.assertRaises(ValueError):
+            bytes_to_word((0x56,))
+            
+    def test_too_many(self):
+        with self.assertRaises(ValueError):
+            bytes_to_word((0x56, 0x43, 0x77))
+            
+    def test_truncate_bytes(self):
+        self.assertEqual(bytes_to_word((0xFF56, 0x43)), 0x4356)
+        self.assertEqual(bytes_to_word((0x56, 0xFF43)), 0x4356)
+        
+class WordToBytesTests(unittest.TestCase):
+    def test_simple(self):
+        self.assertEqual(word_to_bytes(0x5643), (0x43, 0x56))
+        
+    def test_negative(self):
+        with self.assertRaises(ValueError):
+            word_to_bytes(-1)
+            
+    def test_too_big(self):
+        with self.assertRaises(ValueError):
+            word_to_bytes(0x10000)
+            
