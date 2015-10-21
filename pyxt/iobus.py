2bb923a95ea12bc78b97bd18403d398ada5f5cc1
Andy Stamp <luigi88s@gmail.com>
2015-10-21 00:17:22 -0400

    pyxt.iobus: Add support for 8259 initialization sequence.

--- pyxt/iobus.py
+++ pyxt/iobus.py
@@ -19,7 +19,11 @@ class IOComponent(object):
         raise NotImplementedError
         
     def clock(self):
-        """ Handle one iteration of the system clock tick. """
+        """
+        Handle one iteration of the system clock tick.
+        
+        This is not required to be implemented as not all devices have a clock input.
+        """
         pass
         
     def read_byte(self, address):
@@ -40,31 +44,92 @@ class IOComponent(object):
         
 class ProgrammableInterruptController(IOComponent):
     """ An IOComponent emulating an 8259 PIC controller. """
+    EDGE_TRIGGERED = 0
+    LEVEL_TRIGGERED = 1
+    
     def __init__(self, base, **kwargs):
         super(ProgrammableInterruptController, self).__init__(**kwargs)
         self.base = base
+        
+        # Configuration "registers".
+        self.cascade = False
         self.mask = 0x00
+        self.trigger_mode = self.EDGE_TRIGGERED
+        self.priorities = [0, 1, 2, 3, 4, 5, 6, 7]
+        self.vector_base = 0x00
+        self.i8086_8088_mode = False
+        self.auto_eoi = False
+        
+        # ICWS (Initialization Commands Words) state machine, per the datasheet.
+        # 0 indicates the idle state, 1-4 indicate what byte will be processed next.
+        self.icws_state = 0
+        self.icw4_needed = False
         
     def get_address_list(self):
         return [x for x in xrange(self.base, self.base + 2)]
         
-    def read_byte(self, address):
-        offset = address - self.base
-        if offset == 0 or offset == 1 or offset == 2:
-            return self.channels[offset]
-        elif offset == 3:
-            print "CONTROL REGISTER"
-        else:
-            raise ValueError("Bad offset to the 8253!!!")
+    def start_initialization_sequence(self):
+        self.mask = 0x00
+        self.trigger_mode = self.EDGE_TRIGGERED
+        self.priorities[7] = 7
+        self.icws_state = 1
+        self.icw4_needed = False
+        
+    def process_icws_byte(self, value):
+        if self.icws_state == 1:
+            self.icw4_needed = value & 0x01 == 0x01
+            self.cascade = value & 0x02 == 0x02
+            self.trigger_mode = self.LEVEL_TRIGGERED if value & 0x04 == 0x04 else self.EDGE_TRIGGERED
+            
+            if not self.icw4_needed:
+                self.i8086_8088_mode = False
+                self.auto_eoi = False
+                
+            self.icws_state = 2
+            
+        elif self.icws_state == 2:
+            self.vector_base = value & 0xF1
+            
+            if self.cascade:
+                self.icws_state = 3
+            elif self.icw4_needed:
+                self.icws_state = 4
+            else:
+                self.icws_state = 0
+                
+        elif self.icws_state == 3:
+            # TODO: Something with this byte maybe?!
+            if self.icw4_needed:
+                self.icws_state = 4
+            else:
+                self.icws_state = 0
+                
+        elif self.icws_state == 4:
+            self.i8086_8088_mode = value & 0x01 == 0x01
+            self.auto_eoi = value & 0x02 == 0x02
+            
+            self.icws_state = 0
+            
+    # def read_byte(self, address):
+        # offset = address - self.base
+        # if offset == 0 or offset == 1 or offset == 2:
+            # return self.channels[offset]
+        # elif offset == 3:
+            # print "CONTROL REGISTER"
+        # else:
+            # raise ValueError("Bad offset to the 8253!!!")
             
     def write_byte(self, address, value):
         offset = address - self.base
-        if offset == 0:
-            print "CONTROL REGISTER", value
-        elif offset == 1:
-            print "DATA REGISTER", value
-        else:
-            raise ValueError("Bad offset to the 8259!!!")
+        if offset == 0 and value & 0x10 == 0x10:
+            self.start_initialization_sequence()
+        # elif offset == 1:
+            # print "DATA REGISTER", value
+        # else:
+            # raise ValueError("Bad offset to the 8259!!!")
+        
+        if self.icws_state > 0:
+            self.process_icws_byte(value)
             
 class ProgrammableIntervalTimer(IOComponent):
     """ An IOComponent emulating an 8253 PIT timer. """
--- pyxt/tests/test_iobus.py
+++ pyxt/tests/test_iobus.py
@@ -11,4 +11,128 @@ class PICTests(unittest.TestCase):
         
     def test_initial_state(self):
         self.assertEqual(self.obj.mask, 0x00)
+        self.assertEqual(self.obj.priorities, [0, 1, 2, 3, 4, 5, 6, 7])
+        self.assertEqual(self.obj.icws_state, 0)
+        self.assertEqual(self.obj.icw4_needed, False)
+        self.assertEqual(self.obj.trigger_mode, ProgrammableInterruptController.EDGE_TRIGGERED)
+        
+    # ***** ICW1 Tests *****
+    def test_icw1_starts_icws_state_machine(self):
+        self.obj.icws_state = 8
+        self.obj.icw4_needed = True
+        self.obj.trigger_mode = 5
+        self.obj.write_byte(0x00A0, 0x10)
+        self.assertEqual(self.obj.icws_state, 2)
+        self.assertEqual(self.obj.icw4_needed, False)
+        self.assertEqual(self.obj.trigger_mode, ProgrammableInterruptController.EDGE_TRIGGERED)
+        
+    def test_icw1_needs_icw4(self):
+        self.obj.write_byte(0x00A0, 0x11)
+        self.assertEqual(self.obj.icws_state, 2)
+        self.assertEqual(self.obj.icw4_needed, True)
+        
+        # Setting bit 4 and address 0 should restart the init sequence.
+        self.obj.write_byte(0x00A0, 0x10)
+        self.assertEqual(self.obj.icws_state, 2)
+        self.assertEqual(self.obj.icw4_needed, False)
+        
+    def test_icw1_single_vs_cascade(self):
+        self.obj.write_byte(0x00A0, 0x12)
+        self.assertEqual(self.obj.icws_state, 2)
+        self.assertEqual(self.obj.cascade, True)
+        
+        # Setting bit 4 and address 0 should restart the init sequence.
+        self.obj.write_byte(0x00A0, 0x10)
+        self.assertEqual(self.obj.icws_state, 2)
+        self.assertEqual(self.obj.cascade, False)
+        
+    def test_icw1_trigger_mode(self):
+        self.obj.write_byte(0x00A0, 0x14)
+        self.assertEqual(self.obj.icws_state, 2)
+        self.assertEqual(self.obj.trigger_mode, ProgrammableInterruptController.LEVEL_TRIGGERED)
+        
+        # Setting bit 4 and address 0 should restart the init sequence.
+        self.obj.write_byte(0x00A0, 0x10)
+        self.assertEqual(self.obj.icws_state, 2)
+        self.assertEqual(self.obj.trigger_mode, ProgrammableInterruptController.EDGE_TRIGGERED)
+        
+    # ***** ICW2 Tests *****
+    def test_icw2_set_vector_base(self):
+        self.obj.write_byte(0x00A0, 0x10)
+        self.obj.write_byte(0x00A1, 0xFF)
+        self.assertEqual(self.obj.vector_base, 0xF1)
+        
+    def test_icw2_skip_icw3_and_icw4(self):
+        self.obj.write_byte(0x00A0, 0x10)
+        self.obj.write_byte(0x00A1, 0xFF)
+        self.assertEqual(self.obj.icws_state, 0)
+        
+    def test_icw2_skip_icw3_not_icw4(self):
+        self.obj.write_byte(0x00A0, 0x11)
+        self.obj.write_byte(0x00A1, 0xFF)
+        self.assertEqual(self.obj.icws_state, 4)
+        
+    # The next 2 tests ensure the states transition in the proper priority.
+    def test_icw2_to_icw3_and_icw4(self):
+        self.obj.write_byte(0x00A0, 0x13)
+        self.obj.write_byte(0x00A1, 0xFF)
+        self.assertEqual(self.obj.icws_state, 3)
+        
+    def test_icw2_to_icw3_skip_icw4(self):
+        self.obj.write_byte(0x00A0, 0x12)
+        self.obj.write_byte(0x00A1, 0xFF)
+        self.assertEqual(self.obj.icws_state, 3)
+        
+    # ***** ICW3 Tests *****
+    def test_icw3_to_icw4(self):
+        self.obj.write_byte(0x00A0, 0x13)
+        self.obj.write_byte(0x00A1, 0xFF)
+        self.obj.write_byte(0x00A1, 0x00)
+        self.assertEqual(self.obj.icws_state, 4)
+        
+    def test_icw3_skip_icw4(self):
+        self.obj.write_byte(0x00A0, 0x12)
+        self.obj.write_byte(0x00A1, 0xFF)
+        self.obj.write_byte(0x00A1, 0x00)
+        self.assertEqual(self.obj.icws_state, 0)
+        
+    # ***** ICW4 Tests *****
+    def test_icw4_ends_init_sequence(self):
+        self.obj.write_byte(0x00A0, 0x13)
+        self.obj.write_byte(0x00A1, 0xFF)
+        self.obj.write_byte(0x00A1, 0x00)
+        self.obj.write_byte(0x00A1, 0x00)
+        self.assertEqual(self.obj.icws_state, 0)
+        
+    def test_icw4_8086_8088_vs_mcs80_8085_mode(self):
+        self.obj.write_byte(0x00A0, 0x11) # Skip ICW3
+        self.obj.write_byte(0x00A1, 0xFF)
+        self.obj.write_byte(0x00A1, 0x00)
+        self.assertEqual(self.obj.i8086_8088_mode, False)
+        
+        self.obj.write_byte(0x00A0, 0x11) # Skip ICW3
+        self.obj.write_byte(0x00A1, 0xFF)
+        self.obj.write_byte(0x00A1, 0x01)
+        self.assertEqual(self.obj.i8086_8088_mode, True)
+        
+        # Ensure MCS-80/8085 when skipped.
+        self.obj.write_byte(0x00A0, 0x10) # Skip ICW3 and ICW4
+        self.obj.write_byte(0x00A1, 0xFF)
+        self.assertEqual(self.obj.i8086_8088_mode, False)
+        
+    def test_icw4_eoi_mode(self):
+        self.obj.write_byte(0x00A0, 0x11) # Skip ICW3
+        self.obj.write_byte(0x00A1, 0xFF)
+        self.obj.write_byte(0x00A1, 0x00)
+        self.assertEqual(self.obj.auto_eoi, False)
+        
+        self.obj.write_byte(0x00A0, 0x11) # Skip ICW3
+        self.obj.write_byte(0x00A1, 0xFF)
+        self.obj.write_byte(0x00A1, 0x02)
+        self.assertEqual(self.obj.auto_eoi, True)
+        
+        # Ensure normal EOI when skipped.
+        self.obj.write_byte(0x00A0, 0x10) # Skip ICW3 and ICW4
+        self.obj.write_byte(0x00A1, 0xFF)
+        self.assertEqual(self.obj.auto_eoi, False)
         
