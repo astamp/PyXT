17999a4d294b44f1110ea331a46de32a51e1f674
Andy Stamp <luigi88s@gmail.com>
2015-11-27 01:48:39 -0500

    pyxt.ui: Add support for reading the backup font out of a ROM BIOS image and use that on the screen.

--- pyxt/ui.py
+++ pyxt/ui.py
@@ -118,6 +118,9 @@ PYGAME_KEY_TO_XT_SCANCODES = {
 
 assert len(PYGAME_KEY_TO_XT_SCANCODES) == 83
 
+BLACK = (0x00, 0x00, 0x00)
+GREEN = (0x00, 0xC0, 0x00)
+
 # Functions
 
 # Classes
@@ -143,16 +146,75 @@ class XTHardware(object):
         else:
             print event
             
+class CharacterGenerator(object):
+    """ Generates glyphs for a given character. """
+    def blit_character(self, surface, location, index):
+        """ Place a character onto a surface at the given location. """
+        raise NotImplementedError
+        
+    @property
+    def char_width(self):
+        """ Returns the width of characters in pixels. """
+        raise NotImplementedError
+        
+    @property
+    def char_height(self):
+        """ Returns the width of characters in pixels. """
+        raise NotImplementedError
+        
+class CharacterGeneratorBIOS(CharacterGenerator):
+    """ Character generator that uses the 8x8 backup glyph set in the PC BIOS. """
+    FONT_OFFSET = 0xFA6E
+    RESIDENT_CHARS = 128
+    CHAR_WIDTH_BYTES = 1
+    CHAR_WIDTH_PIXELS = CHAR_WIDTH_BYTES * 8
+    CHAR_HEIGHT_PIXELS = 8
+    
+    def __init__(self, bios_file):
+        self.font_data = pygame.Surface((8 * self.RESIDENT_CHARS, 8))
+        self.font_data.fill(BLACK)
+        pix = pygame.PixelArray(self.font_data)
+        
+        with open(bios_file, "rb") as fileptr:
+            fileptr.seek(self.FONT_OFFSET)
+            for index in xrange(self.RESIDENT_CHARS):
+                for row in xrange(0, self.CHAR_HEIGHT_PIXELS):
+                    byte = ord(fileptr.read(1))
+                    for bit in xrange(self.CHAR_WIDTH_PIXELS):
+                        if (1 << (self.CHAR_WIDTH_PIXELS - bit)) & byte:
+                            pix[(index * self.CHAR_WIDTH_PIXELS) + bit, row] = GREEN
+                            
+        # Make sure to explicitly del this to free the surface lock.
+        del pix
+        
+    def blit_character(self, surface, location, index):
+        if index >= self.RESIDENT_CHARS:
+            return
+        surface.blit(self.font_data, location, area = (8 * index, 0, 8, 8))
+        
+    @property
+    def char_width(self):
+        return 8
+        
+    @property
+    def char_height(self):
+        return 8
+        
 # Main application
 def main():
-    print "XTUI"
+    print "PyXT UI"
     pygame.init()
     
-    size = width, height = 640, 480
+    size = width, height = 720, 350
     screen = pygame.display.set_mode(size)
+    screen.fill(BLACK)
     
     hw = XTHardware()
+    cg = CharacterGeneratorBIOS(sys.argv[1])
+    print "LOADING FONT..."
     
+    print "GO"
+    cursor = [0, 0]
     while True:
         for event in pygame.event.get():
             # print event
@@ -160,6 +222,19 @@ def main():
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 hw.handle_keydown(event)
+                
+                try:
+                    char = ord(event.unicode.encode("ascii"))
+                except Exception:
+                    continue
+                    
+                cg.blit_character(screen, cursor, char)
+                pygame.display.flip()
+                cursor[0] += cg.char_width
+                if cursor[0] > 720:
+                    cursor[0] = 0
+                    cursor[1] += cg.char_height
+                    
             elif event.type == pygame.KEYUP:
                 hw.handle_keyup(event)
                 
