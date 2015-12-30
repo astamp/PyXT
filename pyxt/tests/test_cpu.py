a134ae61a57512b6f0d320ec5c71e380bf68c90a
Andy Stamp <luigi88s@gmail.com>
2015-12-30 15:39:00 -0500

    pyxt.tests.test_cpu: Add beginnings of CPU acceptance testing framework.

--- pyxt/tests/test_cpu.py
+++ pyxt/tests/test_cpu.py
@@ -1,6 +1,9 @@
 import unittest
 
+from pyxt.constants import *
 from pyxt.cpu import *
+from pyxt.bus import SystemBus
+from pyxt.memory import RAM
 
 class FlagsRegisterTest(unittest.TestCase):
     def setUp(self):
@@ -91,4 +94,55 @@ class FlagsRegisterTest(unittest.TestCase):
         self.assertEqual(self.obj.value, FLAGS.SIGN)
         self.obj.sf = False
         self.assertEqual(self.obj.value, 0)
+        
+class BaseOpcodeAcceptanceTests(unittest.TestCase):
+    """
+    Basic acceptance testing framework for the CPU class.
+    
+    Code can be generated with the following NASM command:
+    nasm temp.asm -f bin -o temp.bin
+    """
+    def setUp(self):
+        self.bus = SystemBus()
+        self.cpu = CPU()
+        self.cpu.bus = self.bus
+        self.cpu.regs["CS"] = 0x0000
+        self.cpu.regs["DS"] = 0x0000
+        self.memory = RAM(SIXTY_FOUR_KB)
+        self.bus.install_device(0x0000, self.memory)
+        
+    def load_code(self, *args):
+        """ Load a program into the base memory. """
+        for index, byte in enumerate(args):
+            self.memory.write_byte(index, byte)
+            
+    def run_to_halt(self, max_instructions = 1000):
+        """
+        Run the CPU until it halts, returning the number of instructions executed.
+        
+        If it runs for more than max_instructions the test immediately fails.
+        """
+        instruction_count = 0
+        while not self.cpu.hlt:
+            self.cpu.fetch()
+            instruction_count += 1
+            if instruction_count > max_instructions:
+                self.fail("Runaway deetected, terminated after %d instructions." % max_instructions)
+                
+        return instruction_count
+        
+class AddOpcodeTests(BaseOpcodeAcceptanceTests):
+    def test_add_rm8_r8(self):
+        """
+        add [value], al
+        hlt
+        value:
+            db 1
+        """
+        self.cpu.regs["AL"] = 7
+        self.load_code(0x00, 0x06, 0x05, 0x00, 0xF4, 0x01)
+        self.assertEqual(self.run_to_halt(), 2)
+        self.assertEqual(self.cpu.regs["AL"], 7)
+        self.assertEqual(self.memory.read_byte(0x05), 8)
+        
         
