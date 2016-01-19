927e8b1726610252f756dab2f14eb2a9f1f7efad
Andy Stamp <luigi88s@gmail.com>
2016-01-19 00:03:25 -0500

    pyxt.dma: Implement port access to set/get the address and word count registers.

--- pyxt/dma.py
+++ pyxt/dma.py
@@ -7,13 +7,59 @@ pyxt.dma - Virtual DMA controller for PyXT.
 # PyXT imports
 from pyxt.bus import Device
 
+# Constants
+STATE_SI = 0
+STATE_S0 = 1
+STATE_S1 = 2
+STATE_S2 = 3
+STATE_S3 = 4
+STATE_S4 = 5
+STATE_SW = 6
+STATE_S11 = 7
+STATE_S12 = 8
+STATE_S13 = 9
+STATE_S14 = 10
+STATE_S21 = 11
+STATE_S22 = 12
+STATE_S23 = 13
+STATE_S24 = 14
+
+# Functions
+def write_low(word, byte):
+    """ Writes byte into the low portion of word. """
+    return (word & 0xFF00) | (byte & 0xFF)
+    
+def write_high(word, byte):
+    """ Writes byte into the high portion of word. """
+    return (word & 0x00FF) | ((byte & 0xFF) << 8)
+    
+def read_low(word):
+    """ Reads the low portion of word. """
+    return word & 0x00FF
+    
+def read_high(word):
+    """ Reads the high portion of word. """
+    return (word >> 8) & 0x00FF
+    
 # Classes
+class DmaChannel(object):
+    """ Holds info about DMA channel configuration. """
+    def __init__(self):
+        self.address = 0x0000
+        self.word_count = 0x0000
+        self.base_address = 0x0000
+        self.base_word_count = 0x0000
+        self.mode = 0x00
+        
 class DmaController(Device):
     """ A Device emulating an 8237 DMA controller. """
     
     def __init__(self, base, **kwargs):
         super(DmaController, self).__init__(**kwargs)
         self.base = base
+        self.state = STATE_SI
+        self.low_byte = True
+        self.channels = [DmaChannel() for _unused in xrange(4)]
         
     def get_ports_list(self):
         return [x for x in xrange(self.base, self.base + 16)]
@@ -21,9 +67,49 @@ class DmaController(Device):
     def clock(self):
         pass
         
+    def write_low_high(self, word, value):
+        if self.low_byte:
+            self.low_byte = False
+            return write_low(word, value)
+        else:
+            self.low_byte = True
+            return write_high(word, value)
+            
+    def read_low_high(self, word):
+        if self.low_byte:
+            self.low_byte = False
+            return read_low(word)
+        else:
+            self.low_byte = True
+            return read_high(word)
+            
     def io_read_byte(self, port):
         offset = port - self.base
-        
+        if offset & 0x08:
+            return 0x00
+            
+        else:
+            # Which DMA channel?
+            channel = offset >> 1
+            
+            # Odd values are word count.
+            if offset & 0x01:
+                return self.read_low_high(self.channels[channel].word_count)
+            else:
+                return self.read_low_high(self.channels[channel].address)
+                
     def io_write_byte(self, port, value):
         offset = port - self.base
-        
+        if offset & 0x08:
+            pass
+            
+        else:
+            # Which DMA channel?
+            channel = offset >> 1
+            
+            # Odd values are word count.
+            if offset & 0x01:
+                self.channels[channel].word_count = self.channels[channel].base_word_count = self.write_low_high(self.channels[channel].word_count, value)
+            else:
+                self.channels[channel].address = self.channels[channel].base_address = self.write_low_high(self.channels[channel].address, value)
+                
--- pyxt/tests/test_dma.py
+++ pyxt/tests/test_dma.py
@@ -12,3 +12,100 @@ class DMATests(unittest.TestCase):
                                                      0x0008, 0x0009, 0x000A, 0x000B,
                                                      0x000C, 0x000D, 0x000E, 0x000F])
         
+    def test_initial_state(self):
+        self.assertEqual(self.dma.state, STATE_SI)
+        self.assertEqual(self.dma.low_byte, True)
+        
+    def test_read_low_high(self):
+        self.assertEqual(self.dma.read_low_high(0xCAFE), 0xFE)
+        self.assertEqual(self.dma.read_low_high(0xCAFE), 0xCA)
+        self.assertEqual(self.dma.read_low_high(0xCAFE), 0xFE)
+        
+    def test_write_low_high(self):
+        self.assertEqual(self.dma.write_low_high(0x0000, 0xCE), 0x00CE)
+        self.assertEqual(self.dma.write_low_high(0x00CE, 0xFA), 0xFACE)
+        self.assertEqual(self.dma.write_low_high(0xFACE, 0xDE), 0xFADE)
+        
+    def test_io_write_byte_channel_regs(self):
+        self.dma.io_write_byte(0x0000, 0x01)
+        self.dma.io_write_byte(0x0000, 0x02)
+        self.assertEqual(self.dma.channels[0].address, 0x0201)
+        self.assertEqual(self.dma.channels[0].base_address, 0x0201)
+        
+        self.dma.io_write_byte(0x0001, 0x03)
+        self.dma.io_write_byte(0x0001, 0x04)
+        self.assertEqual(self.dma.channels[0].word_count, 0x0403)
+        self.assertEqual(self.dma.channels[0].base_word_count, 0x0403)
+        
+        self.dma.io_write_byte(0x0002, 0x05)
+        self.dma.io_write_byte(0x0002, 0x06)
+        self.assertEqual(self.dma.channels[1].address, 0x0605)
+        self.assertEqual(self.dma.channels[1].base_address, 0x0605)
+        
+        self.dma.io_write_byte(0x0003, 0x07)
+        self.dma.io_write_byte(0x0003, 0x08)
+        self.assertEqual(self.dma.channels[1].word_count, 0x0807)
+        self.assertEqual(self.dma.channels[1].base_word_count, 0x0807)
+        
+        self.dma.io_write_byte(0x0004, 0x09)
+        self.dma.io_write_byte(0x0004, 0x0A)
+        self.assertEqual(self.dma.channels[2].address, 0x0A09)
+        self.assertEqual(self.dma.channels[2].base_address, 0x0A09)
+        
+        self.dma.io_write_byte(0x0005, 0x0B)
+        self.dma.io_write_byte(0x0005, 0x0C)
+        self.assertEqual(self.dma.channels[2].word_count, 0x0C0B)
+        self.assertEqual(self.dma.channels[2].base_word_count, 0x0C0B)
+        
+        self.dma.io_write_byte(0x0006, 0x0D)
+        self.dma.io_write_byte(0x0006, 0x0E)
+        self.assertEqual(self.dma.channels[3].address, 0x0E0D)
+        self.assertEqual(self.dma.channels[3].base_address, 0x0E0D)
+        
+        self.dma.io_write_byte(0x0007, 0x0F)
+        self.dma.io_write_byte(0x0007, 0x10)
+        self.assertEqual(self.dma.channels[3].word_count, 0x100F)
+        self.assertEqual(self.dma.channels[3].base_word_count, 0x100F)
+        
+    def test_io_read_byte_channel_regs(self):
+        # We should not return the base values, so make sure they are invalid.
+        for channel in self.dma.channels:
+            channel.base_address = 0xFFFF
+            channel.base_word_count = 0xFFFF
+            
+        self.dma.channels[0].address = 0x0011
+        self.dma.channels[0].word_count = 0x2233
+        
+        self.dma.channels[1].address = 0x4455
+        self.dma.channels[1].word_count = 0x6677
+        
+        self.dma.channels[2].address = 0x8899
+        self.dma.channels[2].word_count = 0xAABB
+        
+        self.dma.channels[3].address = 0xCCDD
+        self.dma.channels[3].word_count = 0xEEFF
+        
+        self.assertEqual(self.dma.io_read_byte(0x0000), 0x11)
+        self.assertEqual(self.dma.io_read_byte(0x0000), 0x00)
+        
+        self.assertEqual(self.dma.io_read_byte(0x0001), 0x33)
+        self.assertEqual(self.dma.io_read_byte(0x0001), 0x22)
+        
+        self.assertEqual(self.dma.io_read_byte(0x0002), 0x55)
+        self.assertEqual(self.dma.io_read_byte(0x0002), 0x44)
+        
+        self.assertEqual(self.dma.io_read_byte(0x0003), 0x77)
+        self.assertEqual(self.dma.io_read_byte(0x0003), 0x66)
+        
+        self.assertEqual(self.dma.io_read_byte(0x0004), 0x99)
+        self.assertEqual(self.dma.io_read_byte(0x0004), 0x88)
+        
+        self.assertEqual(self.dma.io_read_byte(0x0005), 0xBB)
+        self.assertEqual(self.dma.io_read_byte(0x0005), 0xAA)
+        
+        self.assertEqual(self.dma.io_read_byte(0x0006), 0xDD)
+        self.assertEqual(self.dma.io_read_byte(0x0006), 0xCC)
+        
+        self.assertEqual(self.dma.io_read_byte(0x0007), 0xFF)
+        self.assertEqual(self.dma.io_read_byte(0x0007), 0xEE)
+        
