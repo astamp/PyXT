2e0d05614a1532d8109a381c77c7bc3ca882cbb6
Andy Stamp <luigi88s@gmail.com>
2016-04-24 13:07:15 -0400

    pyxt.interface: Add docstring to key_pressed().

--- pyxt/interface.py
+++ pyxt/interface.py
@@ -5,12 +5,15 @@ pyxt.interfaces - Interfaces for different device types used by the UI classes.
 # Classes
 class DisplayAdapter(object):
     """ Interface for a PyGame display adapter. """
+    
     def get_resolution(self):
         """ Returns a tuple (width, height) of the display size. """
         raise NotImplementedError
         
 class KeyboardController(object):
     """ Interface for a PyGame keyboard controller. """
+    
     def key_pressed(self, scancode):
+        """ Function called when a scancode is ready for the controller. """
         raise NotImplementedError
         
