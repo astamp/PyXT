163e824642a888d4ca34baa2331779827e48d945
Andy Stamp <luigi88s@gmail.com>
2016-05-07 23:57:35 -0400

    pyxt.fdc: Implement more of the controller reset and status register.

--- pyxt/fdc.py
+++ pyxt/fdc.py
@@ -22,9 +22,27 @@ FDC_DATA = 5
 
 CONTROL_DRIVE0_MOTOR = 0x10 # A:
 CONTROL_DRIVE1_MOTOR = 0x20 # B:
+CONTROL_DRIVE2_MOTOR = 0x40 # C: ???
+CONTROL_DRIVE3_MOTOR = 0x80 # D: ???
 CONTROL_DMA_ENABLE = 0x08
 CONTROL_N_RESET = 0x04
 CONTROL_DRIVE_SELECT = 0x03 # Mask for drive number.
+
+STATUS_DRIVE0_BUSY = 0x01
+STATUS_DRIVE1_BUSY = 0x02
+STATUS_DRIVE2_BUSY = 0x04
+STATUS_DRIVE3_BUSY = 0x08
+STATUS_FDC_BUSY = 0x10
+STATUS_NON_DMA_MODE = 0x20
+STATUS_DIRECTION = 0x40
+STATUS_READY = 0x80
+
+# Use these with STATUS_DIRECTION.
+DIRECTION_FDC_TO_CPU = 0x40
+DIRECTION_CPU_TO_FDC = 0x00
+
+# Command states.
+ST_READY = 0
 
 # Classes
 class FloppyDisketteController(Device):
@@ -32,15 +50,19 @@ class FloppyDisketteController(Device):
         super(FloppyDisketteController, self).__init__(**kwargs)
         self.base = base
         self.enabled = False
+        self.state = ST_READY
         
     # Device interface.
     def get_ports_list(self):
         return [self.base + FDC_CONTROL, self.base + FDC_STATUS, self.base + FDC_DATA]
         
+    def reset(self):
+        self.state = ST_READY
+        
     def io_read_byte(self, port):
         offset = port - self.base
         if offset == FDC_STATUS:
-            return 0
+            return self.read_status_register()
         elif offset == FDC_DATA:
             return 0
         else:
@@ -58,5 +80,15 @@ class FloppyDisketteController(Device):
             
     # Local functions.
     def write_control_register(self, value):
+        previously_enabled = self.enabled
         self.enabled = value & CONTROL_N_RESET == CONTROL_N_RESET
+        
+        if self.enabled and not previously_enabled:
+            self.reset()
+        
+    def read_status_register(self):
+        value = 0x00
+        if self.enabled:
+            value |= STATUS_READY
+        return value
         
--- pyxt/tests/test_fdc.py
+++ pyxt/tests/test_fdc.py
@@ -5,14 +5,48 @@ from pyxt.fdc import *
 class FDCTests(unittest.TestCase):
     def setUp(self):
         self.fdc = FloppyDisketteController(0x3F0)
+        self.reset_called = False
         
     def test_address_list(self):
         self.assertEqual(self.fdc.get_ports_list(), [0x3F2, 0x3F4, 0x3F5])
         
     def test_initial_state(self):
         self.assertFalse(self.fdc.enabled)
+        self.assertEqual(self.fdc.state, ST_READY)
+        
+    def test_reset(self):
+        self.fdc.state = 5643
+        self.fdc.reset()
+        self.assertEqual(self.fdc.state, ST_READY)
         
     def test_enable_fdc(self):
         self.fdc.io_write_byte(0x3F2, 0x04)
         self.assertTrue(self.fdc.enabled)
+        
+    def _reset_hook(self):
+        self.reset_called = True
+        
+    def test_enabling_fdc_calls_reset(self):
+        self.fdc.reset = self._reset_hook
+        
+        self.fdc.io_write_byte(0x3F2, 0x04)
+        self.assertTrue(self.reset_called)
+        
+    def test_reset_not_called_if_already_enabled(self):
+        self.fdc.reset = self._reset_hook
+        
+        self.fdc.enabled = True
+        self.fdc.io_write_byte(0x3F2, 0x04)
+        self.assertFalse(self.reset_called)
+        
+    def test_status_register_in_reset(self):
+        self.assertEqual(self.fdc.io_read_byte(0x3F4), 0x00)
+        
+    def test_status_register_after_reset(self):
+        self.fdc.io_write_byte(0x3F2, 0x04)
+        self.assertEqual(self.fdc.io_read_byte(0x3F4), 0x80)
+        
+    def test_status_register_after_reset(self):
+        self.fdc.io_write_byte(0x3F2, 0x04)
+        self.assertEqual(self.fdc.io_read_byte(0x3F4), 0x80)
         
