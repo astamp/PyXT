54a921defc9d21c9098e90bc2920217465432755
Andy Stamp <luigi88s@gmail.com>
2017-02-05 14:49:03 -0500

    pyxt.parallel: Add more of the base parallel port/device functionality.

--- pyxt/parallel.py
+++ pyxt/parallel.py
@@ -3,6 +3,7 @@ pyxt.parallel - Centronics/IEEE1284 parallel port for PyXT.
 
 http://retired.beyondlogic.org/spp/parallel.htm
 https://en.wikipedia.org/wiki/Parallel_port
+http://pinouts.ru/ParallelPorts/ParallelPC_pinout.shtml
 See PORTS.B from Ralf Brown's Interrupt List for more info. (http://www.cs.cmu.edu/~ralf/files.html)
 """
 
@@ -20,13 +21,49 @@ log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
 # Constants
+LPT1_BASE = 0x3BC
+LPT2_BASE = 0x378
+LPT3_BASE = 0x278
+
+LPT1_IRQ = LPT2_IRQ = 7
+LPT3_IRQ = 5
+
+DATA_PORT = 0
+STATUS_PORT = 1
+CONTROL_PORT = 2
+
+STATUS_ERROR = 0x08
+STATUS_SELECT = 0x10
+STATUS_PAPER_OUT = 0x20
+STATUS_ACK = 0x40
+STATUS_BUSY = 0x80
+
+CONTROL_N_STROBE = 0x01
+CONTROL_N_AUTO_LINEFEED = 0x02
+CONTROL_INITIALIZE = 0x04
+CONTROL_N_SELECT_PRINTER = 0x08
+CONTROL_IRQ_ENABLE = 0x10
 
 # Classes
+class ParallelDevice(object):
+    """ Device that is connected to a parallel port. """
+    def __init__(self):
+        self.adapter = None
+        
+    def write_byte(self, value):
+        """ Function called when a byte is written to the data port. """
+        raise NotImplementedError
+        
 class ParallelAdapter(Device):
     def __init__(self, base, irq, **kwargs):
         super(ParallelAdapter, self).__init__(**kwargs)
         self.base = base
         self.irq = irq
+        
+        self.last_data_byte = 0x00
+        self.irq_enable = False
+        
+        self.device = None
         
     # Device interface.
     def get_ports_list(self):
@@ -34,9 +71,22 @@ class ParallelAdapter(Device):
         
     def io_read_byte(self, port):
         offset = port - self.base
-        return 0x00
-        
+        if offset == DATA_PORT:
+            return self.last_data_byte
+        else:
+            return 0x00
+            
     def io_write_byte(self, port, value):
         offset = port - self.base
-        
+        if offset == DATA_PORT:
+            self.last_data_byte = value
+            if self.device is not None:
+                self.device.write_byte(value)
+        elif offset == CONTROL_PORT:
+            self.irq_enable = value & CONTROL_IRQ_ENABLE == CONTROL_IRQ_ENABLE
             
+    def connect(self, device):
+        """ Connects a parallel device to this adapter. """
+        self.device = device
+        self.device.adapter = self
+        
--- pyxt/tests/test_parallel.py
+++ pyxt/tests/test_parallel.py
@@ -3,6 +3,14 @@ import unittest
 from pyxt.tests.utils import SystemBusTestable
 from pyxt.parallel import *
 
+class ParallelDeviceSpy(ParallelDevice):
+    def __init__(self):
+        super(ParallelDeviceSpy, self).__init__()
+        self.last_write_byte = 0x00
+    
+    def write_byte(self, value):
+        self.last_write_byte = value
+        
 class ParallelPortTests(unittest.TestCase):
     def setUp(self):
         self.lpt = ParallelAdapter(0x3BC, 7)
@@ -12,4 +20,37 @@ class ParallelPortTests(unittest.TestCase):
         
     def test_address_list(self):
         self.assertEqual(self.lpt.get_ports_list(), [0x3BC, 0x3BD, 0x3BE])
+        
+    def test_initial_state(self):
+        self.assertEqual(self.lpt.last_data_byte, 0x00)
+        self.assertFalse(self.lpt.irq_enable)
+        
+    def test_read_back_write_data(self):
+        self.lpt.io_write_byte(0x3BC, 0xA5)
+        self.assertEqual(self.lpt.io_read_byte(0x3BC), 0xA5)
+        
+    def test_irq_enable(self):
+        self.assertFalse(self.lpt.irq_enable)
+        self.lpt.io_write_byte(0x3BE, 0x10)
+        self.assertTrue(self.lpt.irq_enable)
+        self.lpt.io_write_byte(0x3BE, 0x00)
+        self.assertFalse(self.lpt.irq_enable)
+        
+    @unittest.skip("Not implemented yet.")
+    def test_control_port_initial_state(self):
+        self.assertEqual(self.lpt.io_read_byte(0x3BE), 0x0B)
+        
+class ParallelPortDeviceTests(unittest.TestCase):
+    def setUp(self):
+        self.lpt = ParallelAdapter(0x3BC, 7)
+        
+        self.dev = ParallelDeviceSpy()
+        self.lpt.connect(self.dev)
+        
+        self.bus = SystemBusTestable()
+        self.bus.install_device(None, self.lpt)
+        
+    def test_write_to_device(self):
+        self.lpt.io_write_byte(0x3BC, 0xA5)
+        self.assertEqual(self.dev.last_write_byte, 0xA5)
         
