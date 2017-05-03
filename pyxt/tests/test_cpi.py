0afcec4db25b963e903d38bf822e602b65c23460
Andy Stamp <luigi88s@gmail.com>
2017-05-03 01:13:50 -0400

    [tests] Remove ega.cpi dependency for tests that don't need it

--- pyxt/tests/test_cpi.py
+++ pyxt/tests/test_cpi.py
@@ -18,10 +18,6 @@ def get_cpi_file(suite, filename):
     )
 
 class CPIFileTests(unittest.TestCase):
-    def setUp(self):
-        self.cpi = CodePageInformationFile()
-        self.cpi.load_from_file(get_cpi_file(self, "ega.cpi"))
-        
     def test_initial_state(self):
         blank_cpi = CodePageInformationFile()
         self.assertIsNone(blank_cpi.font_data)
@@ -39,9 +35,12 @@ class CPIFileTests(unittest.TestCase):
         self.assertEqual(test_cpi.font_data.tell(), 58880) # Size of file.
         
     def test_load_from_file(self):
-        self.assertIsNotNone(self.cpi.font_data)
-        self.cpi.font_data.seek(0, os.SEEK_END)
-        self.assertEqual(self.cpi.font_data.tell(), 58880) # Size of file.
+        test_cpi = CodePageInformationFile()
+        test_cpi.load_from_file(get_cpi_file(self, "ega.cpi"))
+        
+        self.assertIsNotNone(test_cpi.font_data)
+        test_cpi.font_data.seek(0, os.SEEK_END)
+        self.assertEqual(test_cpi.font_data.tell(), 58880) # Size of file.
         
     def test_invalid_font_id_byte(self):
         test_cpi = CodePageInformationFile()
