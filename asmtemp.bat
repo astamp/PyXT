dde6272a3e5f78b01bf4b7feba6b602f3fa1e024
Andy Stamp <luigi88s@gmail.com>
2022-10-20 14:59:29 -0400

    [tools] Dump the hex-with-spaces output for unit tests

--- asmtemp.bat
+++ asmtemp.bat
@@ -3,6 +3,10 @@
 nasm temp.asm -f bin -o temp.bin
 IF ERRORLEVEL 1 GOTO END
 
+echo ========================================================================
 ndisasm temp.bin
+echo ========================================================================
+python -c "import binascii; raw = open('temp.bin', 'rb').read(); data = binascii.hexlify(raw).decode('ascii').upper(); print(' '.join([data[x : x + 2] for x in range(0, len(data), 2)]))"
+echo ========================================================================
 
 :END
